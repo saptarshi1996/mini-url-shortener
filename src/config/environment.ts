5db013bb2de50b5ce7cd9e6230cbccbed9efdde8
Saptarshi Sengupta <saptarshisg96@gmail.com>
2024-08-05 03:22:40 +0530

    auth middleware expiration

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -4,7 +4,8 @@ config()
 
 const environment = {
   SERVER_PORT: process.env.SERVER_PORT as string,
-  SERVER_HOST: process.env.SERVER_HOST as string
+  SERVER_HOST: process.env.SERVER_HOST as string,
+  JWT_SECRET: process.env.JWT_SECRET as string
 }
 
 export default environment
---
+++ src/helpers/auth.ts
@@ -0,0 +1,8 @@
+import { sign } from 'jsonwebtoken'
+import environment from '../config/environment'
+
+export const generateToken = (payload: Record<string, unknown>): string => {
+  return sign(payload, environment.JWT_SECRET, {
+    expiresIn: '1d'
+  })
+}
